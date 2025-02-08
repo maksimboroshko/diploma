825bc1a2308b82ed53938ce36727a11a4e4af1d6
Maksim Boroshko <maksimboroshko211@gmail.com>
2025-02-08 14:09:06 +0300

    Добавил логотип Avito

---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,62 @@
+//package utils;
+//
+//import com.github.javafaker.Faker;
+//
+//public class RandomUtils {
+//
+//    private static final Faker faker = new Faker();
+//
+//    public static String getRandomFirstName() {
+//        return faker.name().firstName();
+//    }
+//
+//    public static String getRandomLastName() {
+//        return faker.name().lastName();
+//    }
+//
+//    public static String getRandomEmail() {
+//        return faker.internet().emailAddress();
+//    }
+//
+//    public static String getRandomPhoneNumber() {
+//        return faker.phoneNumber().cellPhone();
+//    }
+//
+//    public static String getRandomAddress() {
+//        return faker.address().fullAddress();
+//    }
+//
+////    public static String getRandomSchool() {
+////        return faker.education().schoolName();
+////    }
+//
+////    public static String getRandomFieldOfStudy() {
+////        return faker.education().fieldOfStudy();
+////    }
+////
+////    public static String getRandomDegree() {
+////        return faker.education().degree();
+////    }
+//
+//    public static String getRandomStartDate() {
+//        return faker.date().birthday().toString();
+//    }
+//
+//    public static String getRandomEndDate() {
+//        return faker.date().birthday().toString();
+//    }
+//
+//    public static String getRandomHeadline() {
+//        return faker.job().title();
+//    }
+//
+//    public static String getRandomPreferredName() {
+//        return faker.name().firstName(); // Генерируем случайное имя
+//    }
+//
+//    public static String getRandomPreferredSurname() {
+//        return faker.name().lastName(); // Генерируем случайную фамилию
+//    }
+//}
+//
+
